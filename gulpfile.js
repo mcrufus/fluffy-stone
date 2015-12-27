69b95452039c52467b334408e215354c8fca6cbf
Mark Rufus <mcrufus@yandex.ru>
2015-12-27 19:51:22 +0300

    Angular Scope (Week2)

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var gulp = require('gulp'),
     changed = require('gulp-changed'),
     rev = require('gulp-rev'),
     browserSync = require('browser-sync'),
+    ngannotate = require('gulp-ng-annotate'),
     del = require('del');
 
 gulp.task('jshint', function() {
@@ -26,7 +27,7 @@ gulp.task('usemin', ['jshint'], function() {
   return gulp.src('./app/menu.html')
     .pipe(usemin({
       css: [minifycss(), rev()],
-      js: [uglify(), rev()]
+      js: [ngannotate(), uglify(), rev()]
     }))
     .pipe(gulp.dest('dist/'));
 });
